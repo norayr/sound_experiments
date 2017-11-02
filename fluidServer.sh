2bc8dd50d1207c1e26a8ebafc95de11d07e377e7
norayr <norayr@arnet.am>
2017-11-02 17:09:09 +0400

    sf

--- fluidServer.sh
+++ fluidServer.sh
@@ -1,9 +1,9 @@
 DEV=2
-#FNT=SF2/065.8mg_Music_Man_Gm_Gs_V1.0_Bank.sf2
-#FNT=SF2/078.5mg_Melting_Pot_Bank.sf2
-#FNT=SF2/084.0mg_All_In_One_Gm_V1.1_Bank.sf2
-#FNT=SF2/111.2mg_Zx128_Kr5_Bank.sf2
-#FNT=SF2/79.7mg_Marino_Enhanced_Gm_Gs_V1.0_Bank.sf2
-FNT='SF2/GS_sound_set_(16_bit).sf2'
+#FNT=sf2/065.8mg_Music_Man_Gm_Gs_V1.0_Bank.sf2
+#FNT=sf2/078.5mg_Melting_Pot_Bank.sf2
+#FNT=sf2/084.0mg_All_In_One_Gm_V1.1_Bank.sf2
+#FNT=sf2/111.2mg_Zx128_Kr5_Bank.sf2
+#FNT=sf2/79.7mg_Marino_Enhanced_Gm_Gs_V1.0_Bank.sf2
+FNT='sf2/GS_sound_set_(16_bit).sf2'
 
 fluidsynth --server --audio-driver=alsa -o audio.alsa.device=hw:2 $FNT 
