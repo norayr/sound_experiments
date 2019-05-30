840517df581b714b021d3a8d0f57da3e4a9d35ac
noch <noch@hactar.arnet.am>
2019-05-30 23:51:06 +0400

    link

--- bristol.sh
+++ bristol.sh
@@ -1,3 +1,4 @@
+#https://spyurk.am/posts/3226265
 #startBristol -mini -audio alsa -audiodev plughw:2,0
 startBristol  -prophet -audio alsa -audiodev plughw:2,0 -midi alsa -channel 1 -mididev /dev/midi1 &
 sleep 3
