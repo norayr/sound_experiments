6e2eb699d5bdced590895d5acd80c82654b2730a
noch <noch@hactar.arnet.am>
2019-05-30 23:49:52 +0400

    bristol

---
+++ bristol.sh
@@ -0,0 +1,4 @@
+#startBristol -mini -audio alsa -audiodev plughw:2,0
+startBristol  -prophet -audio alsa -audiodev plughw:2,0 -midi alsa -channel 1 -mididev /dev/midi1 &
+sleep 3
+aconnect 20:0 128:0
