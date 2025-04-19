825374bf81eec27b39500af7fd21e017ed93c662
Norayr Chilingarian <norayr@arnet.am>
2025-04-20 00:21:37 +0400

    upload to anonradio script

--- bristol.sh
+++ bristol.sh
@@ -1,5 +1,5 @@
 #https://spyurk.am/posts/3226265
 #startBristol -mini -audio alsa -audiodev plughw:2,0
-startBristol  -prophet -audio alsa -audiodev plughw:2,0 -midi alsa -channel 1 -mididev /dev/midi1 &
+startBristol  -prophet -audio alsa -audiodev plughw:0,0 -midi alsa -channel 1 -mididev /dev/midi1 &
 sleep 3
 aconnect 20:0 128:0
